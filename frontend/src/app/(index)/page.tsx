df60b3ba6488196e1644112e62b48896f4cbeb17
Ian Tateisi <ian.minami.02@gmail.com>
2025-02-28 08:07:47 -0700

    edited page.tsx

--- frontend/src/app/(index)/page.tsx
+++ frontend/src/app/(index)/page.tsx
@@ -1,6 +1,20 @@
+'use client'
+import {HorizontalList} from "@/app/(index)/horizontal-list";
+import {HorizontalListRecommended} from "@/app/(index)/horizontal-list-recommended";
+import {HorizontalListTrailer} from "@/app/(index)/horizontal-list-trailer";
+import {Navbar} from "@/app/components/Navbar";
+import {Footer} from "@/app/components/Footer";
 
-export default function Home() {
+export default function () {
+
     return (
-        <h1 className={"text-3xl font-bold underline"}>Is this thing on?</h1>
-    )
+        <>
+            <Navbar/>
+            <HorizontalListTrailer url={'https://api.jikan.moe/v4/seasons/upcoming?sfw'} title={'Trailers'}/>
+            <HorizontalList url={'https://api.jikan.moe/v4/top/anime?sfw'} title={'Popular Anime'}/>
+            <HorizontalList url={'https://api.jikan.moe/v4/seasons/now?sfw'} title={'Recent Anime'}/>
+            <HorizontalListRecommended url={'https://api.jikan.moe/v4/recommendations/anime?sfw'} title={'Recommended Anime'}/>
+            <Footer/>
+        </>
+    );
 }
